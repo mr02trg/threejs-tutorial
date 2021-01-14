c7d3d4ead5feaaed583cafa92a723390718f5d7f
Tue <manhtuetruong@gmail.com>
2021-01-14 22:54:24 +1100

    sphere and cylinder geometry

--- Section 2/Spheres and Cylinders/spheres-and-cylinders/script.js
+++ Section 2/Spheres and Cylinders/spheres-and-cylinders/script.js
@@ -7,7 +7,7 @@ function init(){
   
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  camera.position.set(0, 0, 8);
+  camera.position.set(0, 0, 10);
   
   const envMap = new THREE.CubeTextureLoader().setPath(`${assetPath}skybox2_`).load(['px.jpg','nx.jpg','py.jpg', 'ny.jpg','pz.jpg', 'nz.jpg']);
   scene.background = envMap;
@@ -27,6 +27,24 @@ function init(){
   
   //Add meshes here 
 
+  // capsule is a group of object
+  capsule = new THREE.Group();
+  const geometry = new THREE.SphereGeometry(2, 30, 20, 0, Math.PI*2, 0, Math.PI/2);
+  const material = new THREE.MeshLambertMaterial({envMap});
+  const sphere = new THREE.Mesh(geometry, material);
+  capsule.add(sphere);
+
+  const geometry2 = new THREE.CylinderGeometry(2, 2, 6, 30, 20, false);
+  const cylinder = new THREE.Mesh(geometry2, material);
+  capsule.add(cylinder);
+
+  const sphere2 = sphere.clone();
+  sphere2.rotation.x = Math.PI;
+  capsule.add(sphere2);
+
+  sphere.position.y = 3;
+  sphere2.position.y = -3;
+  scene.add(capsule);
   
   window.addEventListener( 'resize', resize, false);
   
@@ -37,6 +55,7 @@ function update(){
   requestAnimationFrame( update );
 	renderer.render( scene, camera );
   capsule.rotation.z += 0.01;
+  capsule.rotation.y += 0.01;
 }
 
 function resize(){
